860e4567156e68e103a84e04af725421d9620113
Lynda <Lyndaxj9@users.noreply.github.com>
2017-08-28 18:15:56 -0400

    working on making navbar opacity fade in.  Also removed cup of tea css.

--- js/effect.js
+++ js/effect.js
@@ -1,3 +1,6 @@
+/**
+ * Get the current year from the date for the footer copyright
+ */
 $(document).ready(function(){
         var date = new Date();
         var year = date.getFullYear();
@@ -5,4 +8,34 @@ $(document).ready(function(){
         if(window.innerWidth <= 745) {
           document.getElementById("logo-name").innerHTML = "Omowumi";
         }
+});
+
+/**
+ * Center shrink name and center when screen size decreases
+ */
+var smallestWidth = 745;      
+function navbarLogo() {
+  if(window.innerWidth <= 745) {
+    document.getElementById("logo-name").innerHTML = "Omowumi";
+  } else {
+    document.getElementById("logo-name").innerHTML = "Omowumi Lynda Ademola";
+  }
+}
+      
+window.onresize = navbarLogo;
+
+/**
+ * Change the navbar's opacity when scroll past the main image
+ JS code from: https://codepen.io/michaeldoyle/pen/Bhsif
+ */
+var bgimg = $(document).getElementById("bgimg");
+var navbar = $(document).getElementById("navbar");
+var range = 200;
+$(window).on('scroll', (function() {
+  var scrollTop = $(this).scrollTop(),
+      height = bgimg.outerHeight(),
+      offset = height / 2,
+      calc = 1 - (scrollTop - offset + range) / range;
+  
+  navbar.css({ 'opacity': calc});
 });
